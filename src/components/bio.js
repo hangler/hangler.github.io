90e5d52c171ab7fa572f3fda8ec21dfc6ccddf17
Frank Hangler <frank@plotandscatter.com>
2019-08-07 14:56:07 -0700

    Update bio

--- src/components/bio.js
+++ src/components/bio.js
@@ -5,18 +5,18 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
-import Image from "gatsby-image"
+import React from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
+import Image from 'gatsby-image'
 
-import { rhythm } from "../utils/typography"
+import { rhythm } from '../utils/typography'
 
 const Bio = () => {
   const data = useStaticQuery(graphql`
     query BioQuery {
-      avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
+      avatar: file(absolutePath: { regex: "/profile-pic-3.jpg/" }) {
         childImageSharp {
-          fixed(width: 50, height: 50) {
+          fixed(width: 300, height: 300) {
             ...GatsbyImageSharpFixed
           }
         }
@@ -36,8 +36,7 @@ const Bio = () => {
   return (
     <div
       style={{
-        display: `flex`,
-        marginBottom: rhythm(2.5),
+        marginBottom: rhythm(2.5)
       }}
     >
       <Image
@@ -45,22 +44,63 @@ const Bio = () => {
         alt={author}
         style={{
           marginRight: rhythm(1 / 2),
-          marginBottom: 0,
-          minWidth: 50,
-          borderRadius: `100%`,
-        }}
-        imgStyle={{
-          borderRadius: `50%`,
+          marginBottom: '1rem',
+          minWidth: '300px',
+          marginLeft: 'auto',
+          marginRight: 'auto'
         }}
       />
       <p>
-        Written by <strong>{author}</strong> who lives and works in San
-        Francisco building useful things.
-        {` `}
-        <a href={`https://twitter.com/${social.twitter}`}>
-          You should follow him on Twitter
-        </a>
+        Hi. I’m Frank Hangler, a data designer and developer living in
+        Vancouver, Canada.
       </p>
+      <p>
+        I am founder of <a href='https://plotandscatter.com'>Plot + Scatter</a>,
+        a data communication company specializing in custom-built web-based data
+        visualizations.
+      </p>
+      <p>
+        In 2013-14, I studied at the{' '}
+        <a href='http://www.oii.ox.ac.uk/'>Oxford Internet Institute</a> at the{' '}
+        <a href='http://www.ox.ac.uk/'>University of Oxford</a>, earning a
+        Master’s of Science in the Social Science of the Internet (what a
+        mouthful!). We studied the various social impacts of this relatively new
+        technology — though really, we only scratched the surface. It was a
+        fantastic year that grew my interest in data (Big, Open, and
+        otherwise!), visualization, and smart cities.
+      </p>
+      <p>
+        Before that, I worked at <a href='https://telus.com'>TELUS</a> for
+        several years, building (mostly internal) web applications. I was a
+        member of a development team working with new technologies. Most of my
+        projects were tools to improve internal collaboration.
+      </p>
+      <hr />
+      <h2>Contact</h2>
+      <p>
+        Feel free to email me at{' '}
+        <a href='mailto:fhangler@gmail.com'>fhangler@gmail.com</a>.
+      </p>
+      <p>
+        You can follow me on Twitter at{' '}
+        <a href='https://twitter.com/hangler'>@hangler</a>, though I don't post
+        very much anymore.
+      </p>
+      <hr />
+      <h2>Colophon</h2>
+      <p>
+        The sans and serif fonts used on this site are, respectively, Concourse
+        and Equity, both by Matthew Butterick.
+      </p>
+      <p>
+        The title is not a misspelling, but a{' '}
+        <a href='http://en.wikipedia.org/wiki/Izaak_Walton#The_Compleat_Angler'>
+          reference
+        </a>
+        . (Even though I’ve only gone fishing once, and didn’t catch anything,
+        the pun was too good to pass up.)
+      </p>
+      <hr />
     </div>
   )
 }
